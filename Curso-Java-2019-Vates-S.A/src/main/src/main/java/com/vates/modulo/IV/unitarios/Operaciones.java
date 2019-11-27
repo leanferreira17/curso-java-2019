eebfcd3bf927783995575a5d3cdc24043f57b4b5
Leandro Ivan Ferreira <leanferreira17@outlook.com.ar>
2019-11-27 11:33:36 -0300

    Se agrega material para el ultimo modulo

--- Curso-Java-2019-Vates-S.A/src/main/src/main/java/com/vates/modulo/IV/unitarios/Operaciones.java
+++ Curso-Java-2019-Vates-S.A/src/main/src/main/java/com/vates/modulo/IV/unitarios/Operaciones.java
@@ -2,7 +2,7 @@ package com.vates.modulo.IV.unitarios;
 
 public class Operaciones {
     public int sumar() {
-        int a = 5;
+        int a = 533;
         int b = 46;
 
         int resultado = a + b;
